f9ad815a753a50e11d56dc8bcc837e567f72c489
Nguyen Thien Sang <nguyenthiensang24@gmail.com>
2023-10-09 15:16:31 +0700

    Update pictures

--- HelloGnas/app/src/main/java/com/example/hellognas/MainActivity.kt
+++ HelloGnas/app/src/main/java/com/example/hellognas/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.hellognas
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -30,13 +31,48 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
         val diceRoll2 = dice.roll()
 
-        // Update the screen with the dice roll
+        // Update the screen with the first dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
 
+        val res = when(diceRoll)
+        {
+            1 -> R.drawable.dice_1_b_svg
+            2 -> R.drawable.dice_2_b_svg
+            3 -> R.drawable.dice_3_b_svg
+            4 -> R.drawable.dice_4_b_svg
+            5 -> R.drawable.dice_5_b_svg
+            6 -> R.drawable.dice_6_b_svg
+            else -> 0
+        }
+
+        val resultImageView: ImageView = findViewById(R.id.imageView)
+        resultImageView.setImageResource(res)
+
+        //
+
+
+        // Update the screen with the second dice roll
+
         val resultTextView2: TextView = findViewById(R.id.textView2)
         resultTextView2.text = diceRoll2.toString()
+
+        val res2 = when(diceRoll2)
+        {
+            1 -> R.drawable.dice_1_b_svg
+            2 -> R.drawable.dice_2_b_svg
+            3 -> R.drawable.dice_3_b_svg
+            4 -> R.drawable.dice_4_b_svg
+            5 -> R.drawable.dice_5_b_svg
+            6 -> R.drawable.dice_6_b_svg
+            else -> 0
+        }
+
+        val resultImageView2: ImageView = findViewById(R.id.imageView2)
+        resultImageView2.setImageResource(res2)
     }
+
+
 }
 
 class Dice(private val numberOfSides: Int) {
